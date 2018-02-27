ca8bf73cfadaa99dd7bdb42060b030b7febb8f18
lizgab <36232173+lizgab@users.noreply.github.com>
2018-02-27 13:53:03 +0000

    calculates phase

--- realdata.py
+++ realdata.py
@@ -5,28 +5,23 @@ Created on Tue Feb 27 10:47:21 2018
 """
 
 import numpy as np
-import math
+import cmath
 
 #function to read in data 
 def readfile(filename):
     #open file
     filedata = open(filename, 'r')
     #create blank arrays 
-    timestamp, antenna1, antenna2, vis, visuncert, num, flag = ([] for i in range(7))
+    timestamp, antenna1, antenna2, vis, visuncert, num, flag, phs = ([] for i in range(8))
     #while not at the end...
     while True:
         line = filedata.readline() #read the lines
         if not line: break #end infinite loop if no more lines
-        
-        
-
-                
-                
-               
+                                   
         items = line.split(',') #split the items in each line by ','
-       
-
-        if any("False" in s for s in items):
+        
+        #only take unflagged  
+        if any("False" in x for x in items):
             timestamp.append(float(items[0]))
             antenna1.append(float(items[1]))
             antenna2.append(float(items[2]))
@@ -35,10 +30,7 @@ def readfile(filename):
             num.append(float(items[5]))
             flag.append(str(items[6])) #flag is a string   
         
-        
-    
-    
-                    
+                  
     #turn into normal numpy arrays
     timestamp=np.array(timestamp)
     antenna1=np.array(antenna1)
@@ -46,14 +38,19 @@ def readfile(filename):
     vis=np.array(vis)
     visuncert=np.array(visuncert)
     num=np.array(num)
-    #flag=np.array(flag)
     
+    
+    #calculate phase
+    for i in range (0,len(vis)):
+        phs.append(cmath.phase(vis[i])) #calculate phase
+    #turn from list in nnumpy array 
+    phs=np.array(phs)
 
     
-    return timestamp, antenna1, antenna2, vis, visuncert, num, flag
+    return timestamp, antenna1, antenna2, vis, visuncert, num, flag, phs
 
 #Main
-timestamp, antenna1, antenna2, vis, visuncert, num, flag = readfile('A75_data.dat')
+timestamp, antenna1, antenna2, vis, visuncert, num, flag, phs = readfile('A75_data.dat')
 
 
 
