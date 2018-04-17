c27ef69429ac31b5fcae689bf9ec08bf164ac756
Abbotale <36261768+Abbotale@users.noreply.github.com>
2018-04-17 11:00:34 +0100

    Update RealDataGP.py

--- RealDataGP.py
+++ RealDataGP.py
@@ -31,9 +31,7 @@ def readfile(filename):
         tAv.append(float(items[0]))
         pAv.append(float(items[1]))
         
-    #turn into normal numpy arrays
-    tAv=np.array(tAv)
-    pAv=np.array(pAv)
+
     return tAv, pAv
 
 
@@ -42,20 +40,17 @@ def readfile(filename):
 #Main
 #PARAMETERS TO CHANGE 
 baselineNum = 0
+sampleRate = 100
 
 #read in data from selected baseline
 tAv, pAv = readfile("{}datafile.txt".format(baselineNum))
 
-#Convert to arrays
-pAv = np.array(pAv)
-tAv = np.array(tAv)
-
-#length = len(tAv)
-#theta2= tAv[0]-tAv[length-1]
-#print (theta2)
+#TEST STUFF - REDUCE SIZE OF DATASET FOR USE IN OPTIMIZAION
+tAv = tAv[0:200]
+pAv = pAv[0:200]
 
 #Now fit this data using a Guassian model
-#CALCULATE COVARIANCE MATRIX - Use relevent kernals For now, use two exp kernels
+#CALCULATE COVARIANCE MATRIX - Use relevent kernals
 
 # Squared exponential kernel, takes into account long term rise
 k1 = 0.1**2 * kernels.RationalQuadraticKernel(log_alpha = (0.0)**2, metric=(23280)**2)
@@ -79,49 +74,35 @@ gp = george.GP(kernel, mean=np.mean(pAv), fit_mean=True)
 gp.compute(tAv)
 print(gp.log_likelihood(pAv))
 
-#"""
-#******************************************************************
+
 #NOW OPTIMISING PARAMETERS 
 #theres an optimising function in the scipy library
 #need to feed in a function to optimise and the gradient of the function
 #the function to optimise is the log liklihood, which is on the george library 
 # Define the objective function (negative log-likelihood in this case).
-#def nll(prob):
-#    gp.set_parameter_vector(prob)
-#    ll = gp.log_likelihood(pAv, quiet=True)
-#    return -ll if np.isfinite(ll) else 1e25
-## And the gradient of the objective function.
-#def grad_nll(prob):
-#    gp.set_parameter_vector(prob)
-#    return -gp.grad_log_likelihood(pAv, quiet=True)
-## You need to compute the GP once before starting the optimization.
-#gp.compute(tAv)
-## Print the initial ln-likelihood.
-#print(gp.log_likelihood(pAv))
-## Run the optimization routine.
-##initial parameter estimates
-#p0 = gp.get_parameter_vector()
-##feed into optimisation routine 
-#results = op.minimize(nll, p0, jac=grad_nll, method="L-BFGS-B")
-## Update the kernel and print the final log-likelihood.
-#gp.set_parameter_vector(results.tAv)
-#print(gp.log_likelihood(pAv))
+def nll(prob):
+    gp.set_parameter_vector(prob)
+    ll = gp.log_likelihood(pAv, quiet=True)
+    return -ll if np.isfinite(ll) else 1e25
+# And the gradient of the objective function.
+def grad_nll(prob):
+    gp.set_parameter_vector(prob)
+    return -gp.grad_log_likelihood(pAv, quiet=True)
 
-#"""
-
-#plot results
-#plt.figure()
-
+# Run the optimization routine.
+# Initial parameter estimates
+p0 = gp.get_parameter_vector()
+# Feed into optimisation routine
+results = op.minimize(nll, p0, jac=grad_nll, method="L-BFGS-B")
+# Update the kernel and print the final log-likelihood. (results.x are the optimized hyperparams)
+gp.set_parameter_vector(results.x)
+print(gp.log_likelihood(pAv))
 
 #plot out results 
-x = np.linspace(min(tAv), max(tAv), 2000)
-
-
-
 #Produce an array which contains time stamps where there should be a datapoint
+x = np.linspace(min(tAv), max(tAv), sampleRate)
 
-
-#Predict the values of mean and variance at the locations where there should be data
+#Predict the values of mean and variance at the chosen point
 mu, var = gp.predict(pAv, x, return_var=True)
 std = np.sqrt(var)
 
@@ -133,16 +114,6 @@ plt.fill_between(x, mu+std, mu-std, color="g", alpha=0.5)
 plt.legend()
 
 
-
-
-
-
-
-
-
-
-
-#
 #
 #def gapfile(filename):
 #    #open file
